d7bedb559c864db765de19fb179c36415e6563c1
Mohammed Ibrahim <obietrice734@gmail.com>
2020-04-12 18:42:55 +0200

    initilize commit
    Signed-off-by: Mohammed Ibrahim <obietrice734@gmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+simple ecommerce system built with:
+1-Laravel. 
+2-Vue js.
+3-Tailwindcss.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <div class="flex items-center justify-center">
+        Ecommerce
+    </div>
+@endsection
